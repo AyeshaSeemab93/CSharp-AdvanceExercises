1bf3cc2f026a628804f7c92386b5ae988d32a50c
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-05-18 02:13:19 +0300

    Complete Code for Exercise 8

--- part_08-007_vehicle_registry/src/Exercise007/LicensePlate.cs
+++ part_08-007_vehicle_registry/src/Exercise007/LicensePlate.cs
@@ -18,5 +18,29 @@ namespace Exercise007
             return country + " " + liNumber;
         }
 
+        public override bool Equals(object compared)
+        {
+            if (this == compared)
+            {
+                return true;
+            }
+            if (compared == null || !this.GetType().Equals(compared.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                LicensePlate comparedPlate = (LicensePlate)compared;
+                return this.country == comparedPlate.country
+                &&
+                this.liNumber == comparedPlate.liNumber;
+            }
+        }
+        public override int GetHashCode()
+        {
+
+            return HashCode.Combine(this.country, this.liNumber);
+        }
+
     }
 }
--- part_08-007_vehicle_registry/src/Exercise007/VehicleRegistry.cs
+++ part_08-007_vehicle_registry/src/Exercise007/VehicleRegistry.cs
@@ -4,13 +4,83 @@ namespace Exercise007
     using System.Collections.Generic;
     public class VehicleRegistry
     {
+        public Dictionary<LicensePlate, string> dict;
         public VehicleRegistry()
         {
+
+            this.dict = new Dictionary<LicensePlate, string>();
         }
 
         public bool Add(LicensePlate licensePlate, string owner)
         {
-            return false;
+            if (this.dict.ContainsKey(licensePlate))
+            {
+                if (dict[licensePlate] == null)
+                {
+                    dict[licensePlate] = owner;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+                dict[licensePlate] = owner; // == dict.Add(licenseplate, owner);
+            return true;
         }
+
+        public string Get(LicensePlate licensePlate)
+        {
+            if (dict.ContainsKey(licensePlate))
+            {
+                return this.dict[licensePlate];
+            }
+            else
+            {
+                return "Not registered!";
+            }
+        }
+        public bool Remove(LicensePlate licensePlate)
+        {
+            if (this.dict.ContainsKey(licensePlate))
+            {
+                this.dict.Remove(licensePlate);
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public void PrintLicensePlates()
+        {
+            foreach (KeyValuePair<LicensePlate, string> kpv in this.dict)
+            {
+                Console.WriteLine(kpv.Key);
+            }
+            // Dictionary<LicensePlate, String>.KeyCollection key = dict.Keys;
+            // foreach(LicensePlate plates in key)
+            // {
+            //     Console.WriteLine(plates.ToString());
+            // }
+        }
+
+        public void PrintOwners()
+        {
+            List<string> already = new List<string>();
+            foreach (KeyValuePair<LicensePlate, string> kpv in dict)
+            {
+                if (!already.Contains(kpv.Value))
+                {
+                    Console.WriteLine(kpv.Value);
+                }
+                already.Add(kpv.Value);
+            }
+
+        }
+
+
+
+
     }
 }
